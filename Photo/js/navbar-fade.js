61765804da4b5bd7f12dad09d5642489039f8420
indy1515 <indy1_thailand@hotmail.com>
2014-08-30 00:46:11 +0700

    Update

---
+++ Photo/js/navbar-fade.js
@@ -0,0 +1,24 @@
+$(function() {
+	$(window).scroll(function(){
+		var scrollTop = $(window).scrollTop();
+		if(scrollTop != 0)
+			$('.navbar-fixed-top').stop().animate({'opacity':'0.5'},400);
+		else	
+			$('.navbar-fixed-top').stop().animate({'opacity':'1'},400);
+	});
+	
+	$('.navbar-fixed-top .container-fluid').hover(
+		function (e) {
+			var scrollTop = $(window).scrollTop();
+			if(scrollTop != 0){
+				$('.navbar-fixed-top').stop().animate({'opacity':'1'},400);
+			}
+		},
+		function (e) {
+			var scrollTop = $(window).scrollTop();
+			if(scrollTop != 0){
+				$('.navbar-fixed-top').stop().animate({'opacity':'0.5'},400);
+			}
+		}
+	);
+});
